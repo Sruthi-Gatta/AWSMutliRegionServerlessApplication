eefc8051a94445c42e1148ccd31107abc0fca307
Sruthi-Gatta <sruthi.gatta@sjsu.edu>
2021-10-27 19:51:48 +0000

    228 HW 4 first commit

--- 2_UI/src/environments/environment.ts
+++ 2_UI/src/environments/environment.ts
@@ -8,15 +8,15 @@ export const environment = {
 
   // TODO: This id can be retrieved in output section of the cognito ui
   // cloud formation stack.
-  cognitoIdentityPoolId: '',
+  cognitoIdentityPoolId: 'eu-west-1:c708b58b-25ad-47ce-841f-b8bddf8ddb3e',
 
   // TODO: Facebook app id can be retrieved from the application in your
   // facebook developer account.
-  facebookAppId: '',
+  facebookAppId: '2646364428992678',
 
   // TODO: The API URL is available in the API Gateway console under Stage
   // NOTE: don't forget trailing "/"  For example:
   // https://api.example.com/prod/
-  ticketAPI: ''
+  ticketAPI: 'https://api.arc315wildrydes.com/'
 
 };
